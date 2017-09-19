4f84863432f607f19d9df4125f9eec7989973a13
Diego Silva <diegosiuniube@gmail.com>
2017-09-18 21:04:28 -0300

    fixes

---
+++ app/src/main/java/com/anhanguera/fragmentoslayouts/MainActivity.java
@@ -0,0 +1,32 @@
+package com.anhanguera.fragmentoslayouts;
+
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity
+        implements Fragmento1.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.painelConteudo, Fragmento1.newInstance("","")).commit();
+            }
+        });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
